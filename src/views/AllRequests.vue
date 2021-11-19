4198c5e463add9b2177928c3ad3c341ffd80d381
Evgenii Petrovich <parapheen6@gmail.com>
2021-11-19 19:36:57 +0300

    pagination done

--- src/views/AllRequests.vue
+++ src/views/AllRequests.vue
@@ -4,7 +4,7 @@
         <div v-for="request in reviewRequests" :key="request.id">
             <ReviewRequestCard @click="amplitude.getInstance().logEvent('request-card-click-from-all-requests');" active :reviewRequest="request" :author="request.author!" />
         </div>
-        <n-pagination on-update:page="loadMore" v-model:page="page" :page-count="pageCount" style="margin: 2rem auto 0 auto; justify-content: center;"/>
+        <n-pagination v-model:page="pageNumber" :page-count="pageCount" style="margin: 2rem auto 0 auto; justify-content: center;"/>
     </n-space>
     <n-empty v-else description="Nothing found :(" style="margin-top: 4rem;" />
 </section>
@@ -12,7 +12,7 @@
 
 <script setup lang="ts">
 import { useLoadingBar, useMessage } from 'naive-ui';
-import { onMounted, ref, Ref } from 'vue';
+import { onMounted, ref, Ref, watch } from 'vue';
 import { getReviewRequests } from '../api/reviewRequests.api';
 import { IReviewRequest } from '../interfaces/reviewRequest';
 import ReviewRequestCard from '../components/ReviewRequestCard.vue';
@@ -22,13 +22,13 @@ const reviewRequests = ref(null) as Ref<Array<IReviewRequest> | null>
 
 const loading = useLoadingBar()
 const message = useMessage()
-const page = ref(1)
+const pageNumber = ref(1)
 const pageCount = ref(10)
-const pageSize = 2
+const pageSize = 5
 
 onMounted(async () => {
     loading.start()
-    await getReviewRequests(page.value, pageSize)
+    await getReviewRequests(pageNumber.value, pageSize)
     .then((resp) => {
         reviewRequests.value = resp.data
         pageCount.value = Math.ceil(Number(resp.headers["x-total-count"]) / pageSize)
@@ -39,18 +39,17 @@ onMounted(async () => {
     loading.finish()
 })
 
-const loadMore = async () => {
+watch(pageNumber, async (newPage) => {
     loading.start()
-    await getReviewRequests(page.value, pageSize)
+    await getReviewRequests(newPage, pageSize)
     .then((resp) => {
-        console.log(resp)
         reviewRequests.value = resp.data
     })
     .catch((err) => {
         message.error(err.message)
     })
     loading.finish()
-}
+})
 
 amplitude.getInstance().logEvent('all-requests');
 
