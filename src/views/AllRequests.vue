e47610e6d1f4436f17f8cbb6d9829461b8a51838
Evgenii Petrovich <parapheen6@gmail.com>
2021-11-19 17:11:17 +0300

    pagination work

--- src/views/AllRequests.vue
+++ src/views/AllRequests.vue
@@ -4,7 +4,7 @@
         <div v-for="request in reviewRequests" :key="request.id">
             <ReviewRequestCard @click="amplitude.getInstance().logEvent('request-card-click-from-all-requests');" active :reviewRequest="request" :author="request.author!" />
         </div>
-        <n-pagination v-model:page="page" :page-count="pageCount" style="margin: 2rem auto 0 auto;"/>
+        <n-pagination on-update:page="loadMore" v-model:page="page" :page-count="pageCount" style="margin: 2rem auto 0 auto; justify-content: center;"/>
     </n-space>
     <n-empty v-else description="Nothing found :(" style="margin-top: 4rem;" />
 </section>
@@ -24,20 +24,33 @@ const loading = useLoadingBar()
 const message = useMessage()
 const page = ref(1)
 const pageCount = ref(10)
+const pageSize = 2
 
 onMounted(async () => {
     loading.start()
-    await getReviewRequests()
+    await getReviewRequests(page.value, pageSize)
     .then((resp) => {
         reviewRequests.value = resp.data
-        console.log(resp)
-        pageCount.value = Number(resp.headers["Total-Count"])
+        pageCount.value = Math.ceil(Number(resp.headers["x-total-count"]) / pageSize)
     })
     .catch((err) => {
         message.error(err.message)
     })
     loading.finish()
 })
+
+const loadMore = async () => {
+    loading.start()
+    await getReviewRequests(page.value, pageSize)
+    .then((resp) => {
+        console.log(resp)
+        reviewRequests.value = resp.data
+    })
+    .catch((err) => {
+        message.error(err.message)
+    })
+    loading.finish()
+}
 
 amplitude.getInstance().logEvent('all-requests');
 
